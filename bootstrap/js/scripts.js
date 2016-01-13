f700ba81cc894fdd56dc1bc78e0caa4575d080a5
aundra miller and jonathan sweeney <miller.aundra@gmail.com>
2016-01-13 11:53:55 -0800

    create webpage and styles

---
+++ bootstrap/js/scripts.js
@@ -0,0 +1,10 @@
+$(function() {
+  // var age = parseInt(prompt("How old are you? Really?"));
+  //
+  // if(age >= 18) {
+  //   $("#voters").show();
+  // } else {
+  //   $("#minors").show();
+  // }
+
+});
