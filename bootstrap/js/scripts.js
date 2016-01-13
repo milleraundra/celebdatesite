53c740d60957ed25d2f56a0cc8662c389bc66b4b
aundra miller and jonathan sweeney <jonsweeney26@gmail.com>
2016-01-13 13:48:04 -0800

    add coditions

--- bootstrap/js/scripts.js
+++ bootstrap/js/scripts.js
@@ -1,10 +1,41 @@
-$(function() {
-  // var age = parseInt(prompt("How old are you? Really?"));
-  //
-  // if(age >= 18) {
-  //   $("#voters").show();
-  // } else {
-  //   $("#minors").show();
-  // }
+$(document).ready(function() {
+  $("form#info").submit(function(event) {
+    var gender = $("select#gender").val();
+    var age = $("input#age").val();
+    var shoeSize = $("select#shoe-size").val();
 
+    var celeb = null;
+
+    //for male users
+    if(gender === 'male') {
+      if(age <= 30 && shoeSize !== 'small') {
+        celeb = 'Jennifer Lawrence'; //JL hates small feet
+
+      } else if( age <=30) {
+        celeb = 'Miley Cyrus'
+      } else {  //age condition
+        celeb = 'Helen Mirren';
+      }
+
+
+      //for female users
+    } else {
+
+      if(age <= 40) {
+        celeb = 'Brad Pitt';
+      }else if(age <=65) {
+        celeb = 'Harrison Ford';
+      }
+      else {
+        celeb = 'Patrick Stewart';
+      }
+
+    }
+
+    //Celeb Revealed
+    $("#celeb").empty().append(celeb);
+    $("#result").show();
+
+    event.preventDefault();
+  });
 });
